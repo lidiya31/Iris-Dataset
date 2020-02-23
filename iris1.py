59b8716a9928ae33370f63cfe2abb26306b27921
lidiya31 <61379948+lidiya31@users.noreply.github.com>
2020-02-23 17:57:37 +0530

    Add files via upload

---
+++ iris1.py
@@ -0,0 +1,186 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# In[7]:
+
+
+iris = pd.read_csv("C:\lidiya\Iris.csv") 
+
+
+# In[8]:
+
+
+iris.head(2) 
+
+
+# In[9]:
+
+
+iris.info()
+
+
+# In[10]:
+
+
+fig = iris[iris.Species=='Iris-setosa'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='orange', label='Setosa')
+iris[iris.Species=='Iris-versicolor'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='blue', label='versicolor',ax=fig)
+iris[iris.Species=='Iris-virginica'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='green', label='virginica', ax=fig)
+fig.set_xlabel("Petal Length")
+fig.set_ylabel("Petal Width")
+fig.set_title(" Petal Length VS Width")
+fig=plt.gcf()
+fig.set_size_inches(10,6)
+plt.show()
+
+
+# In[11]:
+
+
+iris.drop('Id',axis=1,inplace=True)
+
+
+# In[12]:
+
+
+fig = iris[iris.Species=='Iris-setosa'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='orange', label='Setosa')
+iris[iris.Species=='Iris-versicolor'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='blue', label='versicolor',ax=fig)
+iris[iris.Species=='Iris-virginica'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='green', label='virginica', ax=fig)
+fig.set_xlabel("Petal Length")
+fig.set_ylabel("Petal Width")
+fig.set_title(" Petal Length VS Width")
+fig=plt.gcf()
+fig.set_size_inches(10,6)
+plt.show()
+
+
+# In[13]:
+
+
+iris.hist(edgecolor='black', linewidth=1.2)
+fig=plt.gcf()
+fig.set_size_inches(12,6)
+plt.show()
+
+
+# In[14]:
+
+
+# importing alll the necessary packages to use the various classification algorithms
+from sklearn.linear_model import LogisticRegression  # for Logistic Regression algorithm
+from sklearn.cross_validation import train_test_split #to split the dataset for training and testing
+from sklearn.neighbors import KNeighborsClassifier  # for K nearest neighbours
+from sklearn import svm  #for Support Vector Machine (SVM) Algorithm
+from sklearn import metrics #for checking the model accuracy
+from sklearn.tree import DecisionTreeClassifier #for using Decision Tree Algoithm
+
+
+# In[15]:
+
+
+iris.shape
+
+
+# In[16]:
+
+
+plt.figure(figsize=(7,4)) 
+sns.heatmap(iris.corr(),annot=True,cmap='cubehelix_r') #draws  heatmap with input as the correlation matrix calculted by(iris.corr())
+plt.show()
+
+
+# In[17]:
+
+
+train, test = train_test_split(iris, test_size = 0.3)# in this our main data is split into train and test
+# the attribute test_size=0.3 splits the data into 70% and 30% ratio. train=70% and test=30%
+print(train.shape)
+print(test.shape)
+
+
+# In[19]:
+
+
+train_X = train[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']]# taking the training data features
+train_y=train.Species# output of our training data
+test_X= test[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']] # taking test data features
+test_y =test.Species   #output value of test data
+
+
+# In[20]:
+
+
+train_X.head(2)
+
+
+# In[21]:
+
+
+test_X.head(2)
+
+
+# In[22]:
+
+
+train_y.head()  ##output of the training data
+
+
+# In[23]:
+
+
+model = svm.SVC() #select the algorithm
+model.fit(train_X,train_y) # we train the algorithm with the training data and the training output
+prediction=model.predict(test_X) #now we pass the testing data to the trained algorithm
+print('The accuracy of the SVM is:',metrics.accuracy_score(prediction,test_y))#now we check the accuracy of the algorithm. 
+#we pass the predicted output by the model and the actual output
+
+
+# In[24]:
+
+
+model = LogisticRegression()
+model.fit(train_X,train_y)
+prediction=model.predict(test_X)
+print('The accuracy of the Logistic Regression is',metrics.accuracy_score(prediction,test_y))
+
+
+# In[25]:
+
+
+model=DecisionTreeClassifier()
+model.fit(train_X,train_y)
+prediction=model.predict(test_X)
+print('The accuracy of the Decision Tree is',metrics.accuracy_score(prediction,test_y))
+
+
+# In[26]:
+
+
+model=KNeighborsClassifier(n_neighbors=3) #this examines 3 neighbours for putting the new data into a class
+model.fit(train_X,train_y)
+prediction=model.predict(test_X)
+print('The accuracy of the KNN is',metrics.accuracy_score(prediction,test_y))
+
+
+# In[27]:
+
+
+a_index=list(range(1,11))
+a=pd.Series()
+x=[1,2,3,4,5,6,7,8,9,10]
+for i in list(range(1,11)):
+    model=KNeighborsClassifier(n_neighbors=i) 
+    model.fit(train_X,train_y)
+    prediction=model.predict(test_X)
+    a=a.append(pd.Series(metrics.accuracy_score(prediction,test_y)))
+plt.plot(a_index, a)
+plt.xticks(x)
+
